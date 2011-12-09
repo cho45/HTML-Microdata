7800cc5508c312d2444fc5d325a0ee7474c77fdf
cho45 <cho45@lowreal.net>
2011-12-09 15:03:43 +0900

    init

---
+++ lib/HTML/Microdata.pm
@@ -0,0 +1,38 @@
+package HTML::Microdata;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+HTML::Microdata - 
+
+=head1 SYNOPSIS
+
+  use HTML::Microdata;
+
+
+=head1 DESCRIPTION
+
+HTML::Microdata is 
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
